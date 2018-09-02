68cd5061550b0133d50959c39245d25244c737a5
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-09-02 16:53:46 -0700

    playing with routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  // console.log('hitting / route');
+  // res.send('Hey! It works!');
+  res.send(req.query);
 });
+
+router.get('/reverse/:name', (req, res) => {
+  res.send('hey it works');
+})
 
 module.exports = router;
