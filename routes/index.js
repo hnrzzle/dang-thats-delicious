27fe414f158542bb95ece64c9667e17ec02e3629
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-09-02 17:45:43 -0700

    use mvc arch

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
 // Do work here
-router.get('/', (req, res) => {
-  // console.log('hitting / route');
-  // res.send('Hey! It works!');
-  res.render('hello', { name: 'henry', dog: 'lua' });
-});
+router.get('/', storeController.homePage);
 
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join('');
-  console.log(reverse);
-  res.send(reverse);
-});
 
 module.exports = router;
